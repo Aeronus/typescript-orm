cc6971c6a7b48e507937e4c8360901c0f770e2a3
lettau.pascal <p.lettau@mwparts.com>
2023-08-16 12:57:40 +0200

    style: line wrapping

--- src/decorator/EntityDecorator.ts
+++ src/decorator/EntityDecorator.ts
@@ -3,17 +3,13 @@ import { ClassConstructor } from 'class-transformer';
 import entityMetadataStore from '../metadata/EntityMetadataStore';
 import { EntityInterface } from '../entity/EntityInterface';
 
-type EntityDecoratorParams<
-    EntityType extends EntityInterface,
-    RepositoryType extends EntityRepository<EntityType>,
-> = {
+type EntityDecoratorParams<EntityType extends EntityInterface, RepositoryType extends EntityRepository<EntityType>> = {
     repositoryClass: ClassConstructor<RepositoryType>;
 };
 
-export function Entity<
-    EntityType extends EntityInterface,
-    RepositoryType extends EntityRepository<EntityType>,
->({ repositoryClass }: EntityDecoratorParams<EntityType, RepositoryType>) {
+export function Entity<EntityType extends EntityInterface, RepositoryType extends EntityRepository<EntityType>>({
+    repositoryClass,
+}: EntityDecoratorParams<EntityType, RepositoryType>) {
     return function (
         constructor: ClassConstructor<EntityType>,
         _context: ClassDecoratorContext<ClassConstructor<EntityType>>,
