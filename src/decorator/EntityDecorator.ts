f10f3ec90da718cd7b12dd1493ade8cac77d6ab7
lettau.pascal <p.lettau@mwparts.com>
2023-08-23 09:37:08 +0200

    feat: add base uri for entity

--- src/decorator/EntityDecorator.ts
+++ src/decorator/EntityDecorator.ts
@@ -5,15 +5,15 @@ import { EntityInterface } from '../entity/EntityInterface';
 
 type EntityDecoratorParams<EntityType extends EntityInterface, RepositoryType extends EntityRepository<EntityType>> = {
     repositoryClass: ClassConstructor<RepositoryType>;
+    baseUri: string;
 };
 
 export function Entity<EntityType extends EntityInterface, RepositoryType extends EntityRepository<EntityType>>({
     repositoryClass,
+    baseUri,
 }: EntityDecoratorParams<EntityType, RepositoryType>) {
-    return function (
-        constructor: ClassConstructor<EntityType>,
-        _context: ClassDecoratorContext<ClassConstructor<EntityType>>,
-    ) {
-        entityMetadataStore.addRepository(constructor, repositoryClass);
+    return function (constructor: ClassConstructor<EntityType>) {
+        entityMetadataStore.setRepository(constructor, repositoryClass);
+        entityMetadataStore.setBaseUri(constructor, baseUri);
     };
 }
--- src/repository/EntityRepository.ts
+++ src/repository/EntityRepository.ts
@@ -2,3 +2,16 @@ import { EntityInterface } from '../entity/EntityInterface';
 
 // @ts-ignore
 export abstract class EntityRepository<EntityType extends EntityInterface> {}
+export abstract class EntityRepository<EntityType extends EntityInterface> {
+    constructor(private baseUri?: string) {}
+
+    protected getUriFromEntity(id?: string | number): string {
+        let uri = this.baseUri || '';
+
+        if (id !== undefined) {
+            uri = `${uri}/${id}`;
+        }
+
+        return uri;
+    }
+}
